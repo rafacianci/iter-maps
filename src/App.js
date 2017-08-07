8d651eae813ebf6618eee7512acc950d0fdae9e3
Rafa Cianci <rafael.fuzifaru@gmail.com>
2017-08-07 17:00:06 -0300

    add router and map container

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
+import Main from './containers/Main/';
 import './App.css';
 
 const App = () => (
-  <div className='App'>
-    <div className='App-header'>
-      <img src={logo} className='App-logo' alt='logo' />
-      <h2>Welcome to React</h2>
+  <Router>
+    <div className='App'>
+      <Route exact path='/' component={Main} />
     </div>
-    <p className='App-intro'>
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
-  </div>
+  </Router>
 );
 
 export default App;
---
+++ src/containers/Main/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+const Main = () => (
+  <div className='container'>
+    <div className='list'>
+      Map
+    </div>
+  </div>
+);
+
+export default Main;
