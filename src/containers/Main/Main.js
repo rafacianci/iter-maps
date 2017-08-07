3103fd4aec0ef776fa72fc2c14a768d9b3064ae6
Rafa Cianci <rafael.fuzifaru@gmail.com>
2017-08-07 17:58:46 -0300

    add maps

--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
 import './style.css';
 
+const AnyReactComponent = ({ text }) => <div className='marker'>{text}</div>;
+
 const Main = () => (
-  <div className='container'>
-    <div className='list'>
-      Map
-    </div>
+  <div className='map-container'>
+    <GoogleMapReact
+      defaultZoom={11}
+      defaultCenter={{ lat: 59.955413, lng: 30.337844 }}
+    >
+      <AnyReactComponent
+        lat={59.955413}
+        lng={30.337844}
+        text='Kreyser Avrora'
+      />
+    </GoogleMapReact>
   </div>
 );
 
